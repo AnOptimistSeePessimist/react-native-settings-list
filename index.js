a3af0fc07ecde8a9df696f9a0dc552e28eda99fe
KWei <46306725+KWei19@users.noreply.github.com>
2019-07-17 10:35:36 +0800

    fix blank when release

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import createReactClass from "create-react-class";
 
 import {
   View,
-  TouchableOpacity,
+  TouchableHighlight,
   Text,
   StyleSheet,
   ScrollView,
@@ -13,9 +14,9 @@ import {
   Switch,
   Image,
   ViewPropTypes
-} from 'react-native';
+} from "react-native";
 
-const ARROW_ICON = require('./img/icon-arrow-settings.png');
+const ARROW_ICON = require("./img/icon-arrow-settings.png");
 
 class SettingsList extends React.Component {
   static propTypes = {
@@ -25,34 +26,37 @@ class SettingsList extends React.Component {
     underlayColor: PropTypes.string,
     defaultTitleStyle: Text.propTypes.style,
     defaultTitleInfoPosition: PropTypes.string,
-    scrollViewProps: PropTypes.object,
+    scrollViewProps: PropTypes.object
   };
 
   static defaultProps = {
-    backgroundColor: 'white',
-    borderColor: 'black',
+    backgroundColor: "white",
+    borderColor: "black",
     defaultItemSize: 50,
-    underlayColor: 'transparent',
+    underlayColor: "transparent",
     defaultTitleStyle: { fontSize: 16 }
   };
 
   _getGroups() {
-    let groupNumber = -1;
+    var groupNumber = -1;
     let headers = [];
     let itemGroup = [];
     let result = [];
-    React.Children.forEach(this.props.children, (child) => {
+    React.Children.forEach(this.props.children, child => {
       // Allow for null, optional fields
       if (!child) return;
 
-      if (child.type.displayName === 'Header') {
+      if (child.type.displayName === "Header") {
         if (groupNumber != -1) {
-          result[groupNumber] = { items: itemGroup, header: headers[groupNumber] };
+          result[groupNumber] = {
+            items: itemGroup,
+            header: headers[groupNumber]
+          };
           itemGroup = [];
         }
         groupNumber++;
         headers[groupNumber] = child.props;
-      } else if (child.type.displayName === 'Item') {
+      } else if (child.type.displayName === "Item") {
         if (groupNumber == -1) {
           groupNumber++;
         }
@@ -75,28 +79,45 @@ class SettingsList extends React.Component {
           return this._groupView(group, index);
         })}
       </ScrollView>
-    )
+    );
   }
 
   _groupView(group, index) {
     if (group.header) {
       return (
-        <View key={'group_' + index}>
-          <Text style={[{ margin: 5 }, group.header.headerStyle]}
-                numberOfLines={group.header.headerNumberOfLines} ellipsizeMode="tail"
-                ref={group.header.headerRef}>{group.header.headerText}</Text>
-          <View style={{ borderTopWidth: 1, borderBottomWidth: 1, borderColor: this.props.borderColor }}>
+        <View key={"group_" + index}>
+          <Text
+            style={[{ margin: 5 }, group.header.headerStyle]}
+            numberOfLines={group.header.headerNumberOfLines}
+            ellipsizeMode="tail"
+            ref={group.header.headerRef}
+          >
+            {group.header.headerText}
+          </Text>
+          <View
+            style={{
+              borderTopWidth: 1,
+              borderBottomWidth: 1,
+              borderColor: this.props.borderColor
+            }}
+          >
             {group.items.map((item, index) => {
               return this._itemView(item, index, group.items.length);
             })}
           </View>
         </View>
-      )
+      );
     } else {
       let items;
       if (group.items.length > 0) {
         items = (
-          <View style={{ borderTopWidth: 1, borderBottomWidth: 1, borderColor: this.props.borderColor }}>
+          <View
+            style={{
+              borderTopWidth: 1,
+              borderBottomWidth: 1,
+              borderColor: this.props.borderColor
+            }}
+          >
             {group.items.map((item, index) => {
               return this._itemView(item, index, group.items.length);
             })}
@@ -104,63 +125,67 @@ class SettingsList extends React.Component {
         );
       }
 
-      return (
-        <View key={'group_' + index}>
-          {items}
-        </View>
-      )
+      return <View key={"group_" + index}>{items}</View>;
     }
   }
 
   _itemEditableBlock(item, index, position) {
-    return ([
+    return [
       <Text
-        key={'itemTitle_' + index}
+        key={"itemTitle_" + index}
         style={[
           item.titleStyle ? item.titleStyle : this.props.defaultTitleStyle,
-          position === 'Bottom' ? null : styles.titleText
-        ]}>
+          position === "Bottom" ? null : styles.titleText
+        ]}
+      >
         {item.title}
       </Text>,
-      item.isEditable ?
+      item.isEditable ? (
         <TextInput
           key={item.id}
-          style={item.editableTextStyle ? item.editableTextStyle : styles.editableText}
+          style={
+            item.editableTextStyle
+              ? item.editableTextStyle
+              : styles.editableText
+          }
           placeholder={item.placeholder}
-          onChangeText={(text) => item.onTextChange(text)}
-          value={item.value} />
-        : null
-    ])
+          onChangeText={text => item.onTextChange(text)}
+          value={item.value}
+        />
+      ) : null
+    ];
   }
 
   _itemTitleBlock(item, index, position) {
-    return ([
+    return [
       <Text
-        key={'itemTitle_' + index}
+        key={"itemTitle_" + index}
         style={[
           item.titleStyle ? item.titleStyle : this.props.defaultTitleStyle,
-          position === 'Bottom' ? null : styles.titleText
-        ]}>
+          position === "Bottom" ? null : styles.titleText
+        ]}
+      >
         {item.title}
       </Text>,
-      item.titleInfo ?
+      item.titleInfo ? (
         <Text
-          key={'itemTitleInfo_' + index}
+          key={"itemTitleInfo_" + index}
           style={[
-            item.rightSideStyle ? item.rightSideStyle
-              :
-              position === 'Bottom' ? null : styles.rightSide,
-            { color: '#B1B1B1' },
+            item.rightSideStyle
+              ? item.rightSideStyle
+              : position === "Bottom" ? null : styles.rightSide,
+            { color: "#B1B1B1" },
             item.titleInfoStyle
-          ]}>
+          ]}
+        >
           {item.titleInfo}
         </Text>
-        : null
-    ])
+      ) : null
+    ];
   }
 
   _itemView(item, index, max) {
-    let border;
+    var border;
 
     if (item.type) {
       return item;
@@ -168,31 +193,70 @@ class SettingsList extends React.Component {
 
     if (item.borderHide) {
       switch (item.borderHide) {
-        case 'Top' :
-          border = { borderBottomWidth: 1, borderColor: this.props.borderColor };
+        case "Top":
+          border = {
+            borderBottomWidth: 1,
+            borderColor: this.props.borderColor
+          };
           break;
-        case 'Bottom' :
+        case "Bottom":
           border = { borderTopWidth: 1, borderColor: this.props.borderColor };
           break;
       }
     } else {
-      border = index === max - 1 ? { borderWidth: 0 } : {
-        borderBottomWidth: 1,
-        borderColor: this.props.borderColor
-      };
+      border =
+        index === max - 1
+          ? { borderWidth: 0 }
+          : { borderBottomWidth: 1, borderColor: this.props.borderColor };
     }
-    let titleInfoPosition = item.titleInfoPosition ? item.titleInfoPosition : this.props.defaultTitleInfoPosition;
+
+    let titleInfoPosition = item.titleInfoPosition
+      ? item.titleInfoPosition
+      : this.props.defaultTitleInfoPosition;
+
     return (
-      <TouchableOpacity accessible={false} key={'item_' + index}
-                        underlayColor={item.underlayColor ? item.underlayColor : this.props.underlayColor}
-                        onPress={item.onPress} onLongPress={item.onLongPress} ref={item.itemRef}>
+      <TouchableHighlight
+        accessible={false}
+        key={"item_" + index}
+        underlayColor={
+          item.underlayColor ? item.underlayColor : this.props.underlayColor
+        }
+        onPress={item.onPress}
+        onLongPress={item.onLongPress}
+        ref={item.itemRef}
+      >
         <View
-          style={item.itemBoxStyle ? item.itemBoxStyle : [styles.itemBox, { backgroundColor: item.backgroundColor ? item.backgroundColor : this.props.backgroundColor }]}>
+          style={
+            item.itemBoxStyle
+              ? item.itemBoxStyle
+              : [
+                  styles.itemBox,
+                  {
+                    backgroundColor: item.backgroundColor
+                      ? item.backgroundColor
+                      : this.props.backgroundColor
+                  }
+                ]
+          }
+        >
           {item.icon}
-          {item.isAuth ?
-            <View style={item.titleBoxStyle ? item.titleBoxStyle : [styles.titleBox, border]}>
-              <View style={{ paddingLeft: 5, flexDirection: 'column', flex: 1 }}>
-                <View style={{ borderBottomWidth: 1, borderColor: this.props.borderColor }}>
+          {item.isAuth ? (
+            <View
+              style={
+                item.titleBoxStyle
+                  ? item.titleBoxStyle
+                  : [styles.titleBox, border]
+              }
+            >
+              <View
+                style={{ paddingLeft: 5, flexDirection: "column", flex: 1 }}
+              >
+                <View
+                  style={{
+                    borderBottomWidth: 1,
+                    borderColor: this.props.borderColor
+                  }}
+                >
                   <TextInput
                     ref="UserNameInputBlock"
                     onSubmitEditing={() => this.refs.PasswordInputBlock.focus()}
@@ -207,36 +271,62 @@ class SettingsList extends React.Component {
                     style={{ flex: 1, height: 30 }}
                     placeholder="password"
                     secureTextEntry={true}
-                    returnKeyType={'go'}
+                    returnKeyType={"go"}
                     {...item.authPropsPW}
                     onSubmitEditing={() => item.onPress()}
                   />
                 </View>
               </View>
             </View>
-            :
+          ) : (
             <View
-              style={item.titleBoxStyle ? item.titleBoxStyle : [styles.titleBox, border, { minHeight: item.itemWidth ? item.itemWidth : this.props.defaultItemSize }]}>
-              {titleInfoPosition === 'Bottom' ?
-                <View style={{ flexDirection: 'column', flex: 1, justifyContent: 'center' }}>
-                  {item.isEditable ? this._itemEditableBlock(item, inde, 'Bottom') : this._itemTitleBlock(item, index, 'Bottom')}
+              style={
+                item.titleBoxStyle
+                  ? item.titleBoxStyle
+                  : [
+                      styles.titleBox,
+                      border,
+                      {
+                        minHeight: item.itemWidth
+                          ? item.itemWidth
+                          : this.props.defaultItemSize
+                      }
+                    ]
+              }
+            >
+              {titleInfoPosition === "Bottom" ? (
+                <View
+                  style={{
+                    flexDirection: "column",
+                    flex: 1,
+                    justifyContent: "center"
+                  }}
+                >
+                  {item.isEditable
+                    ? this._itemEditableBlock(item, inde, "Bottom")
+                    : this._itemTitleBlock(item, index, "Bottom")}
                 </View>
-                : item.isEditable ? this._itemEditableBlock(item, index) : this._itemTitleBlock(item, index)}
+              ) : item.isEditable ? (
+                this._itemEditableBlock(item, index)
+              ) : (
+                this._itemTitleBlock(item, index)
+              )}
 
               {item.rightSideContent ? item.rightSideContent : null}
-              {item.hasSwitch ?
+              {item.hasSwitch ? (
                 <Switch
                   {...item.switchProps}
                   style={styles.rightSide}
-                  onValueChange={(value) => item.switchOnValueChange(value)}
-                  value={item.switchState} />
-                : null}
+                  onValueChange={value => item.switchOnValueChange(value)}
+                  value={item.switchState}
+                />
+              ) : null}
               {this.itemArrowIcon(item)}
             </View>
-          }
+          )}
         </View>
-      </TouchableOpacity>
-    )
+      </TouchableHighlight>
+    );
   }
 
   itemArrowIcon(item) {
@@ -245,37 +335,41 @@ class SettingsList extends React.Component {
     }
 
     if (item.hasNavArrow) {
-      return <Image style={[styles.rightSide, item.arrowStyle]} source={ARROW_ICON} />;
+      return (
+        <Image
+          style={[styles.rightSide, item.arrowStyle]}
+          source={ARROW_ICON}
+        />
+      );
     }
 
     return null;
   }
 }
-
 module.exports = SettingsList;
 
 const styles = StyleSheet.create({
   itemBox: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row'
+    justifyContent: "center",
+    flexDirection: "row"
   },
   titleBox: {
     flex: 1,
     marginLeft: 15,
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   titleText: {
     flex: 1,
-    alignSelf: 'center'
+    alignSelf: "center"
   },
   rightSide: {
     marginRight: 15,
-    alignSelf: 'center'
+    alignSelf: "center"
   },
   editableText: {
     flex: 1,
-    textAlign: 'right',
+    textAlign: "right",
     marginRight: 15
   }
 });
@@ -284,28 +378,31 @@ const styles = StyleSheet.create({
  * Optional Header for groups
  */
 
-SettingsList.Header = class Header extends React.Component {
-  static propTypes = {
+SettingsList.Header = createReactClass({
+  propTypes: {
     headerText: PropTypes.string,
     headerStyle: Text.propTypes.style,
     headerRef: PropTypes.func,
-    headerNumberOfLines: PropTypes.number,
-  };
-
-  static defaultProps = {
-    headerNumberOfLines: 1,
-  };
-
+    headerNumberOfLines: PropTypes.number
+  },
+  getDefaultProps() {
+    return {
+      headerNumberOfLines: 1
+    };
+  },
   /**
    * not directly rendered
    */
   render() {
     return null;
   }
-};
+});
 
-SettingsList.Item = class Item extends React.Component {
-  static propTypes = {
+/**
+ * Individual Items in the Settings List
+ */
+SettingsList.Item = createReactClass({
+  propTypes: {
     /**
      * Title being displayed
      */
@@ -397,19 +494,19 @@ SettingsList.Item = class Item extends React.Component {
      */
     rightSideContent: PropTypes.node,
     /* Gives opens to hide specific borders */
-    borderHide: PropTypes.oneOf(['Top', 'Bottom', 'Both']),
+    borderHide: PropTypes.oneOf(["Top", "Bottom", "Both"]),
 
-    itemRef: PropTypes.func,
-  };
-
-  static defaultProps = {
-    hasNavArrow: true
-  };
-
+    itemRef: PropTypes.func
+  },
+  getDefaultProps() {
+    return {
+      hasNavArrow: true
+    };
+  },
   /**
    * not directly rendered
    */
   render() {
     return null;
   }
-};
+});
